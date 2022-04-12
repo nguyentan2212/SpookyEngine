94e30de7fc0a6a0c4370fd5c083182a2a7c80eef
nguyentan2212 <nguyentan2212@gmail.com>
2022-04-12 14:27:40 +0700

    draw box

--- SpookyEngine/Graphic/Graphic.cpp
+++ SpookyEngine/Graphic/Graphic.cpp
@@ -8,6 +8,12 @@ Graphic::~Graphic()
 	d3ddev->Release();
 	backBuffer->Release();
 	spriteHandler->Release();
+	if (g_pVB != nullptr) {
+		g_pVB->Release();
+	}
+	if (g_pIB != nullptr) {
+		g_pIB->Release();
+	}
 }
 
 shared_ptr<Graphic> Graphic::GetInstance()
@@ -21,6 +27,8 @@ shared_ptr<Graphic> Graphic::GetInstance()
 
 bool Graphic::Initialize(HWND hWnd, int width, int height)
 {
+	g_pVB = NULL;
+
 	windowWidth = width;
 	windowHeight = height;
 
@@ -28,9 +36,6 @@ bool Graphic::Initialize(HWND hWnd, int width, int height)
 	{
 		return false;
 	}
-
-	shared_ptr<Camera2D> camera = Camera2D::GetInstance();
-	camera->Initialize(0, 0, windowWidth, windowHeight);
 
 	return true;
 }
@@ -66,23 +71,85 @@ bool Graphic::InitializeDirectX(HWND hWnd)
 
 void Graphic::BeginRender()
 {
-	/*D3DXMATRIX matScale;
-	D3DXMatrixScaling(&matScale, 2.0f, 2.0f, .0f);
-	spriteHandler->SetTransform(&matScale);*/
-
-	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 
 	d3ddev->BeginScene();
-
-	HRESULT hr = spriteHandler->Begin(D3DXSPRITE_SORT_DEPTH_FRONTTOBACK | D3DXSPRITE_ALPHABLEND);
-	
 }
 
 void Graphic::EndRender()
 {
-	spriteHandler->End();
-
+	// DrawBox(50, 50, 80, 80);
 	d3ddev->EndScene();
 
 	d3ddev->Present(NULL, NULL, NULL, NULL);
+}
+
+void Graphic::BeginSprite()
+{
+	spriteHandler->Begin(D3DXSPRITE_SORT_DEPTH_FRONTTOBACK | D3DXSPRITE_ALPHABLEND);
+}
+
+void Graphic::EndSprite()
+{
+	spriteHandler->End();
+}
+
+void Graphic::DrawBoxFilled(float x, float y, float w, float h, DWORD color)
+{
+	vertex V[4];
+
+	V[0].color = V[1].color = V[2].color = V[3].color = color;
+
+	V[0].z = V[1].z = V[2].z = V[3].z = 1.0f;
+	V[0].rhw = V[1].rhw = V[2].rhw = V[3].rhw = 1.0f;
+
+	V[0].x = x;
+	V[0].y = y;
+	V[1].x = x + w;
+	V[1].y = y;
+	V[2].x = x + w;
+	V[2].y = y + h;
+	V[3].x = x;
+	V[3].y = y + h;
+
+	unsigned short indexes[] = { 0, 1, 3, 1, 2, 3 };
+
+	d3ddev->CreateVertexBuffer(4 * sizeof(vertex), 0, D3DFVF_XYZRHW | D3DFVF_DIFFUSE, D3DPOOL_MANAGED, &g_pVB, NULL);
+	d3ddev->CreateIndexBuffer(6 * sizeof(short), 0, D3DFMT_INDEX16, D3DPOOL_MANAGED, &g_pIB, NULL);
+
+	VOID* pVertices;
+	g_pVB->Lock(0, sizeof(V), (void**)&pVertices, 0);
+	memcpy(pVertices, V, sizeof(V));
+	g_pVB->Unlock();
+
+	VOID* pIndex;
+	g_pIB->Lock(0, sizeof(indexes), (void**)&pIndex, 0);
+	memcpy(pIndex, indexes, sizeof(indexes));
+	g_pIB->Unlock();
+
+	d3ddev->SetStreamSource(0, g_pVB, 0, sizeof(vertex));
+	d3ddev->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
+	d3ddev->SetIndices(g_pIB);
+
+	d3ddev->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 4, 0, 2);
+}
+
+void Graphic::DrawBox(float x, float y, float w, float h, DWORD color)
+{
+	x -= w / 2;
+	y -= h / 2;
+
+	DrawBoxFilled(x, y, w, 1, color);             // Top
+	DrawBoxFilled(x, y + h - 1, w, 1, color);         // Bottom
+	DrawBoxFilled(x, y + 1, 1, h - 2 * 1, color);       // Left
+	DrawBoxFilled(x + w - 1, y + 1, 1, h - 2 * 1, color);   // Right
+}
+
+void Graphic::Draw(shared_ptr<SpriteInfo> info, RECT srcRect ,Vector center, Vector position, D3DXMATRIX transformMatrix)
+{
+	spriteHandler->SetTransform(&transformMatrix);
+
+	D3DXVECTOR3 pCenter = center.ToDirectXVector();
+	D3DXVECTOR3 pPos = position.ToDirectXVector();
+	spriteHandler->Draw(info->texture->GetTexture(), &srcRect, &pCenter, &pPos, D3DCOLOR_XRGB(255, 255, 255));
 }
--- SpookyEngine/Graphic/Graphic.h
+++ SpookyEngine/Graphic/Graphic.h
@@ -3,11 +3,19 @@
 #include <d3dx9.h>
 #include <memory>
 #include <queue>
-#include "../Core/Camera2D.h"
 #include "Drawable2D.h"
+#include "Sprite.h"
 
 using namespace::std;
 
+struct vertex
+{
+	FLOAT x, y, z, rhw;
+	DWORD color;
+};
+
+class Sprite;
+struct SpriteInfo;
 
 class Graphic
 {
@@ -20,19 +28,23 @@ public:
 	bool Initialize(HWND hWnd, int width, int height);
 	void BeginRender();
 	void EndRender();
+	void BeginSprite();
+	void EndSprite();
 
 	LPDIRECT3DDEVICE9 GetDevice() const {
 		return d3ddev;
 	}
 
-	LPD3DXSPRITE GetSpriteHandler()  const {
-		return spriteHandler;
-	}
+	void DrawBoxFilled(float x, float y, float w, float h, DWORD color = D3DCOLOR_XRGB(255, 0, 0));
+	void DrawBox(float x, float y, float w, float h, DWORD color = D3DCOLOR_XRGB(255, 0, 0));
+	void Draw(shared_ptr<SpriteInfo> info, RECT srcRect, Vector center, Vector position, D3DXMATRIX transformMatrix);
 
 protected:
 	LPDIRECT3D9 d3d;            // the pointer to our Direct3D interface
 	LPDIRECT3DDEVICE9 d3ddev;   // the pointer to the device class
 	LPDIRECT3DSURFACE9	backBuffer; // back buffer
+	LPDIRECT3DVERTEXBUFFER9 g_pVB;    // Buffer to hold vertices
+	LPDIRECT3DINDEXBUFFER9  g_pIB;    // Buffer to hold indices
 	LPD3DXSPRITE spriteHandler;
 
 	int windowWidth = 0;
