936a6bc9688b687838829cbfa266a60a54bb1f73
nguyentan2212 <nguyentan2212@gmail.com>
2022-04-12 10:37:25 +0700

    use sprite info for constructor and render

--- SpookyEngine/Graphic/Sprite.cpp
+++ SpookyEngine/Graphic/Sprite.cpp
@@ -1,35 +1,38 @@
 #include "Sprite.h"
 
-Sprite::Sprite(std::shared_ptr<Texture> texture, LPD3DXSPRITE spriteHandler, RECT srcRect, bool isFlipped) : texture(texture),
-	srcRect(srcRect), spriteHandler(spriteHandler), isFlipped(isFlipped), Drawable2D()
+Sprite::Sprite(LPD3DXSPRITE spriteHandler, shared_ptr<SpriteInfo> info, Vector scaleVec)
 {
-	
+	this->spriteHandler = spriteHandler;
+	this->info = info;
+	this->scaleVec = scaleVec;
 }
 
-Sprite::Sprite(std::shared_ptr<Texture> texture, LPD3DXSPRITE spriteHandler, double width, double height, double x, double y, bool isFlipped) :
-	texture(texture), spriteHandler(spriteHandler), isFlipped(isFlipped), Drawable2D()
+Sprite::Sprite(LPD3DXSPRITE spriteHandler, shared_ptr<SpriteInfo> info, double scaleX, double scaleY)
 {
-	srcRect.left = x;
-	srcRect.top = y;
-	srcRect.right = x + width;
-	srcRect.bottom = y + height;
+	this->spriteHandler = spriteHandler;
+	this->info = info;
+	this->scaleVec = Vector(scaleX, scaleY);
 }
 
 void Sprite::Render(Matrix transMat)
 {
 	D3DXMATRIX matScale;
-	if (isFlipped)
-	{
-		D3DXMatrixScaling(&matScale, -1.0f, 1.0f, .0f);
-		transMat = Matrix::Scaling(Vector(-1, 1, 0)) * transMat;
-	}
-	else {
-		D3DXMatrixScaling(&matScale, 1.0f, 1.0f, .0f);
-	}
-	spriteHandler->SetTransform(&matScale);
-	Vector temp = transMat * transform.GetLocalPosition();
-	D3DXVECTOR3 pos = D3DXVECTOR3(temp.GetValueX(), temp.GetValueY(), 0);
-	OutputDebugStringW((L"[Sprite]: " + to_wstring(pos.x) + L", " + to_wstring(pos.y) + L"\n").c_str());
+	D3DXMatrixScaling(&matScale, scaleVec.GetValueX(), scaleVec.GetValueY(), .0f);
+	matScale._43 = zIndex;
+	transMat = Matrix::Scaling(scaleVec) * transMat;
 
-	spriteHandler->Draw(texture->GetTexture(), &srcRect, NULL, &pos, D3DCOLOR_XRGB(255, 255, 255));
+	spriteHandler->SetTransform(&matScale);
+
+	Vector temp = transMat * transform.GetLocalPosition();
+	D3DXVECTOR3 pos = temp.ToDirectXVector();
+	D3DXVECTOR3 center = GetCenter().ToDirectXVector();
+
+	OutputDebugStringW((L"[Sprite]: " + to_wstring(pos.x) + L", " + to_wstring(zIndex) + L"\n").c_str());
+	RECT rect = GetSrcRect();
+	spriteHandler->Draw(info->texture->GetTexture(), &rect, &center, &pos, D3DCOLOR_XRGB(255, 255, 255));
+}
+
+void Sprite::SetZIndex(float zIndex)
+{
+	this->zIndex = zIndex;
 }
--- SpookyEngine/Graphic/Sprite.h
+++ SpookyEngine/Graphic/Sprite.h
@@ -7,28 +7,41 @@
 
 using namespace::std;
 
+struct SpriteInfo {
+	double x, y, width, height;
+	shared_ptr<Texture> texture;
+};
+
 class Sprite : public Drawable2D
 {
 public:
-	Sprite(std::shared_ptr<Texture> texture, LPD3DXSPRITE spriteHandler, RECT srcRect, bool isFlipped = false);
-	Sprite(std::shared_ptr<Texture> texture, LPD3DXSPRITE spriteHandler, double width, double height, double x, double y, bool isFlipped = false);
+	Sprite(LPD3DXSPRITE spriteHandler, shared_ptr<SpriteInfo> info, Vector scaleVec);
+	Sprite(LPD3DXSPRITE spriteHandler, shared_ptr<SpriteInfo> info, double scaleX = 1, double scaleY = 1);
 
 	void Render(Matrix transMat);
+	void SetZIndex(float zIndex);
 
-	double GetWidth() const {
-		return srcRect.right - srcRect.left;
-	}
-	double GetHeight() const {
-		return srcRect.bottom - srcRect.top;
-	}
-	std::shared_ptr<Texture> GetTexture() const {
-		return texture;
+	shared_ptr<Texture> GetTexture() const {
+		return info->texture;
 	}
 
-	bool isFlipped;
+	RECT GetSrcRect() const 
+	{
+		RECT srcRect;
+		srcRect.left = info->x;
+		srcRect.top = info->y;
+		srcRect.right = info->x + info->width;
+		srcRect.bottom = info->y + info->height;
+		return srcRect;
+	}
+
+	Vector GetCenter() const {
+		return Vector(info->width / 2, info->height / 2, 0);
+	}
+
 private:
-	std::shared_ptr<Texture> texture; 
-	RECT srcRect;
+	shared_ptr<SpriteInfo> info;
+	Vector scaleVec;
 	LPD3DXSPRITE spriteHandler;
 };
 
