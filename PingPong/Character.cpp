7f73b7e0568a35a6e54e883a1198201cab86cd7f
nguyentan2212 <nguyentan2212@gmail.com>
2022-04-17 17:56:35 +0700

    add constructor and collision event

--- PingPong/Character.cpp
+++ PingPong/Character.cpp
@@ -1,5 +1,35 @@
 #include "Character.h"
 
+Character::Character(string name, double width, double height, Vector pos) : GameObject(name, width, height, pos)
+{
+	shared_ptr<ResourceLocator> locator = ResourceLocator::GetInstance();
+	shared_ptr<Animation> animation = shared_ptr<Animation>(new Animation());
+
+	// character's animation
+	// left direction
+	for (int i = 0; i < 7; i++)
+	{
+		string spriteName = "ChowmeinConga/idle/" + to_string(i) + ".png";
+		shared_ptr<Sprite> sprite = locator->GetSprite(spriteName, Vector(1, 1));
+		animation->AddFrame(sprite, 50);
+	}
+	animation->SetZIndex(0);
+	AddAnimation(animation);
+
+	// right direction
+	animation = shared_ptr<Animation>(new Animation());
+	for (int i = 0; i < 7; i++)
+	{
+		string spriteName = "ChowmeinConga/idle/" + to_string(i) + ".png";
+		shared_ptr<Sprite> sprite = locator->GetSprite(spriteName, Vector(-1, 1));
+		animation->AddFrame(sprite, 50);
+	}
+	animation->SetZIndex(0);
+	AddAnimation(animation);
+
+	isDrawBox = true;
+}
+
 void Character::Initialize()
 {
 	
@@ -31,6 +61,27 @@ void Character::Update(double delta)
 		transform.Translate(0, -1 * delta);
 	}
 	Vector pos = this->transform.GetLocalPosition();
+	shared_ptr<Grid> grid = Grid::GetInstance();
 
-	OutputDebugStringW((L"[Character]: " + to_wstring(pos.GetValueX()) + L", " + to_wstring(pos.GetValueY()) + L"\n").c_str());
+	//OutputDebugStringW((L"[Character]: " + to_wstring(pos.GetValueX()) + L", " + to_wstring(pos.GetValueY()) + L"\n").c_str());
+}
+
+void Character::Render(Matrix transMat)
+{
+	transMat = transMat * GetWorldTransform();
+	if (state == LEFT)
+	{
+		drawableList[0]->Render(transMat);
+	}
+	else {
+		drawableList[1]->Render(transMat);
+	}
+}
+
+void Character::OnCollision(vector<CollisionEvent> collisionEvents)
+{
+	shared_ptr<GameObject> obj = collisionEvents[0].obj;
+	wstring name = StringConverter::StringToWide(obj->name);
+	Vector pos = GetWorldPosition();
+	OutputDebugStringW((L"[Character]: " + name + L", x = " + to_wstring(pos.GetValueX()) + L"\n").c_str());
 }
--- PingPong/Character.h
+++ PingPong/Character.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <string>
 #include "Core/GameObject.h"
 #include "Keyboard/KeyboardClass.h"
+#include "Core/ResourceLocator.h"
+#include "Utils/StringConverter.h"
 
 #define LEFT -1
 #define RIGHT 1
@@ -11,12 +14,12 @@ using namespace::std;
 class Character : public GameObject
 {
 public: 
-	Character() : GameObject() {}
-	Character(const Vector& position) : GameObject(position) {}
-	Character(const Transform2D& transform) : GameObject(transform) {}
+	Character(string name, double width, double height, Vector pos);
 
 	void Initialize();
 	void Update(double delta);
+	void Render(Matrix transMat);
+	void OnCollision(vector<CollisionEvent> collisionEvents);
 
 private: 
 	int state = RIGHT;
