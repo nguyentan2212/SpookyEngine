621567075a1d77708facfc7ff4c106cf5273a974
nguyentan2212 <nguyentan2212@gmail.com>
2022-04-17 11:30:34 +0700

    fix Getsprite

--- SpookyEngine/Core/ResourceLocator.h
+++ SpookyEngine/Core/ResourceLocator.h
@@ -29,14 +29,14 @@ public:
 		return textures.at(name);
 	}
 
-	shared_ptr<Sprite> GetSprite(string name, Vector scaleVec = Vector(1, 1, 0)) const {
+	shared_ptr<Sprite> GetSprite(string name, Vector scale = Vector(1, 1)) const {
 		shared_ptr<SpriteInfo> info = sprites.at(name);
 		if (info == nullptr)
 		{
 			return nullptr;
 		}
 		shared_ptr<Graphic> graphic = Graphic::GetInstance();
-		shared_ptr<Sprite> sprite = shared_ptr<Sprite>(new Sprite(info, scaleVec));
+		shared_ptr<Sprite> sprite = shared_ptr<Sprite>(new Sprite(info, scale));
 		return sprite;
 	}
 private:
